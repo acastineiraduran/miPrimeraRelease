4f86c002a28b4639c8bab1500703d30a6443d975
Angel Castineira Duran <acastineiraduran@danielcastelao.org>
2022-10-26 10:16:43 +0200

    clase 26 oct

---
+++ src/Clase26Oct.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Clase26Oct {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Escriba su nombre");
+        System.out.println("nome = " + nome);
+    }
+}
