50b761cbf4c33cf0d9e0846787ae4f5544665ced
Angel Castineira Duran <acastineiraduran@danielcastelao.org>
2022-10-26 10:27:00 +0200

    creado el artifact

--- src/Clase26Oct.java
+++ src/Clase26Oct.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 
+/**
+ * @version 1.2
+ */
 public class Clase26Oct {
     public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog("Escriba su nombre");
